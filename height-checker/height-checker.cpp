0ac5d169cf02cbe84f70f753babed3f774efeece
SANDEEP KUMAR AAZAD <121500488+sandeepaazad@users.noreply.github.com>
2024-03-15 21:28:40 +0530

    Time: 5 ms, Memory: 10.1 MB - LeetHub

---
+++ height-checker/height-checker.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int count=0;
+        vector<int>ans=heights;
+        sort(ans.begin(),ans.end());
+        for(int i=0;i<heights.size();i++){
+            if(heights[i]!=ans[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+};
